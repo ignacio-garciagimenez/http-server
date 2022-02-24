76f1f7cffba7e03802a1bd43b656633434690962
Ignacio Garcia Gimenez <ignaciogarciagimenez@gmail.com>
2022-02-24 02:06:37 -0300

    reading until the body

--- src/main/java/com/cleanerj/App.java
+++ src/main/java/com/cleanerj/App.java
@@ -1,5 +1,11 @@
 package com.cleanerj;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
@@ -7,9 +13,28 @@ package com.cleanerj;
 public class App 
 {
     public static void main(String[] args)
-    {
-        System.out.println("Hello World!");
+    {       
 
-        
+        try {
+            ServerSocket server = new ServerSocket(8080);
+            System.out.println("Listening on port 8080...");
+            
+            while(true) {
+                Socket client = server.accept();
+                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                String line;
+                
+                while ((line = in.readLine()) != null && !line.isEmpty() || !client.isConnected()) {
+                    System.out.println(line);                    
+                }
+                System.out.println("Ended");
+                client.close();
+            }
+        } catch (IOException e) {
+            System.out.println("For sure is in use");
+            e.printStackTrace();
+        } finally {
+        }
+
     }
 }
